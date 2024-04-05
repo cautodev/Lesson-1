c0e19b4be2d28c871db96613c74487a304e2ff2c
Mikail <mikailjk@gmail.com>
2024-04-05 14:24:33 -0400

    Fix for failing tests

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -5,8 +5,8 @@ def fibonacci(n):
     if n <= 0:
         return False, []
 
-    fib_sequence = [1, 1]
-    for i in range(2, n):
+    fib_sequence = [0, 1]
+    for _ in range(2, n):
         fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
     return True, fib_sequence
 
